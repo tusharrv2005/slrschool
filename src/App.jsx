ec53eaa7706085b18e4836c1d7ef579ad09e39ff
tusharrv2005 <tusharrverma2005@gmail.com>
2026-05-21 07:57:25 +0530

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-700">
+        Welcome to SLR School
+      </h1>
+    </div>
+  )
+}
